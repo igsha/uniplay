d4e3ee2cf11db0d906bdd6d88ac7a9209fe06087
Igor Sharonov <igsha@users.noreply.github.com>
2025-05-25 21:31:42 +0300

    kodik: New extractor

---
+++ kodik.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+which grep http rg xq fzf tr base64 sed mpv > /dev/null
+
+read -r DOMAIN < <(grep -Po ".+//[^/]+" <<< "$1")
+read -r TITLE < <(http GET "$1" | grep -Po "<title>\K.+(?=</title\>)")
+IFS=$'\t' read -r STITLE URL < <(http GET "$1" \
+    | rg --multiline-dotall -UPo '<div class="serial-series-box".*?</div>' \
+    | xq -r '.div.select.option[]
+| "\(.["@data-title"])\t'$DOMAIN'/ftor?type=seria&id=\(.["@data-id"])&hash=\(.["@data-hash"])"' \
+    | fzf)
+
+read -r VAL < <(http GET "$URL" \
+    | jq -r '.links["720"][0].src')
+
+CAB="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SAB="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+for ((rot=0; rot<25; ++rot)); do
+    rot2=$((rot+1))
+    read -r RES < <(echo "$VAL" \
+        | tr "A-Za-z" "${CAB:$rot2:1}-ZA-${CAB:$rot:1}${SAB:$rot2:1}-za-${SAB:$rot:1}")
+    if [[ "$RES" =~ ^Ly9 ]]; then
+        URL="$(echo "$RES" | base64 -d | sed 's;^//;https://;')"
+        break
+    fi
+done
+
+echo "Play URL: $URL"
+mpv --title="$TITLE - $STITLE" "$URL"
