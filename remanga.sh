f5a65f71a91c5e5244b1942feadb625446c1ee61
Igor Sharonov <igsha@users.noreply.github.com>
2025-06-01 22:46:21 +0300

    remanga: Support caching

--- remanga.sh
+++ remanga.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/env bash
 set -e
 
+which grep http jq fzf parallel pdfcpu xdg-open magick > /dev/null
+
 URL="$1" # E.g.,https://remanga.org/manga/one-punch-man-one
 URL="${URL%%\?*}" # Remove query part
+DIR="$XDG_CACHE_HOME/remanga"
+
+mark() {
+    while IF=$'\t' read -r CHAPTER ID; do
+        printf "$CHAPTER\t$ID"
+        if [[ -d "$1/$ID" ]]; then
+            printf "\t*"
+        fi
+
+        printf "\n"
+    done
+}
 
 read -r DOMAIN < <(grep -Po '.+//[^/]+' <<< "$URL")
 read -r REQNAME < <(grep -Po '.+//[^/]+/[^/]+/\K[^/]+' <<< "$URL")
-mapfile -t < <(http GET "https://api.remanga.org/api/v2/titles/$REQNAME/" \
-    | jq -r '(.secondary_name | sub("/"; "-")),.branches[0].id')
+IFS=$'\t' read -r NAME BRANCHID < <(http GET "https://api.remanga.org/api/v2/titles/$REQNAME/" \
+    | jq -r '"\(.secondary_name | sub("/"; "-"))\t\(.branches[0].id)"')
 
-read -r ID < <(http GET "https://api.remanga.org/api/v2/titles/chapters/?branch_id=${MAPFILE[1]}&ordering=index" \
+IFS=$'\t' read -r CHAPTER ID MARKED < <(http GET "https://api.remanga.org/api/v2/titles/chapters/?branch_id=$BRANCHID&ordering=index" \
     | jq -r '.results[] | "\(.chapter)\t\(.id)"' \
-    | fzf --with-nth=1 \
-    | awk -F'\t' '{print $2}')
+    | mark "$DIR/$NAME" \
+    | fzf)
 
-read -r DIR < <(mktemp -d)
-echo "Tempdir $DIR"
+DIR="$DIR/$NAME/$ID"
+mkdir -p "$DIR"
 
-http GET "https://api.remanga.org/api/v2/titles/chapters/$ID/" \
-    | jq -r '.pages | flatten | .[] | .link' \
-    | parallel -k http GET "{1}" "referer:$DOMAIN" -o "$DIR/{1/}" '&&' echo "$DIR/{1/}" \
-    | xargs pdfcpu import "$DIR/$NAME.pdf"
+PDFFILE="$DIR/$NAME - $CHAPTER.pdf"
+if [[ -z "$MARKED" ]]; then
+    mapfile -t < <(http GET "https://api.remanga.org/api/v2/titles/chapters/$ID/" \
+        | jq -r '.pages | flatten | .[] | .link' \
+        | parallel -k http GET {1} "referer:$DOMAIN" -o \"$DIR/{1/}\" '&&' echo \"$DIR/{1/}\")
+    magick \( "${MAPFILE[@]}" -append \) -crop 1x${#MAPFILE[@]}@ +repage "$DIR"/replaced_%04d.jpg
+    pdfcpu import -c disable "$PDFFILE" "$DIR"/replaced_*.jpg
+fi
 
-xdg-open "$DIR/$NAME.pdf"
-rm -r "$DIR"
+xdg-open "$PDFFILE"
