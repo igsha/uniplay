a9acd76c947f8f4773c4be2d35cead37608a1011
Igor Sharonov <igsha@users.noreply.github.com>
2025-06-05 17:53:39 +0300

    mangalib: Refactor caching

--- mangalib.sh
+++ mangalib.sh
@@ -9,12 +9,12 @@ DIR="$XDG_CACHE_HOME/mangalib"
 
 mark() {
     while IF=$'\t' read -r VOLUME NUMBER REST; do
-        printf "$VOLUME\t$NUMBER\t$REST"
+        MARK="(new)"
         if [[ -d "$1/$VOLUME/$NUMBER" ]]; then
-            printf "\t*"
+            MARK="(cached)"
         fi
 
-        printf "\n"
+        printf "%s\t$VOLUME\t$NUMBER\t$REST\n" "$MARK"
     done
 }
 
@@ -24,7 +24,7 @@ read -r NAME < <(http "https://api.cdnlibs.org/api/$REQNAME" \
     | jq -r .data.eng_name)
 
 NAME="${NAME//\//-}"
-IFS=$'\t' read -r VOLUME NUMBER STITLE MARKED < <(http "https://api.cdnlibs.org/api/$REQNAME/chapters" \
+IFS=$'\t' read -r MARKED VOLUME NUMBER STITLE < <(http "https://api.cdnlibs.org/api/$REQNAME/chapters" \
     | jq -r '.data[] | "\(.volume)\t\(.number)\t\(.name)"' \
     | mark "$DIR/$NAME" \
     | fzf)
@@ -34,12 +34,14 @@ DIR="$DIR/$NAME/$VOLUME/$NUMBER"
 
 mkdir -p "$DIR"
 
-PDFFILE="$DIR/$NAME - $STITLE.pdf"
-if [[ -z "$MARKED" ]]; then
-    http "https://api.cdnlibs.org/api/$REQNAME/chapter?volume=$VOLUME&number=$NUMBER" \
-        | jq -r '.data.pages[] | "https://img33.imgslib.link\(.url)"' \
-        | parallel -k http GET {1} "referer:$DOMAIN" -o \"$DIR/{1/}\" '&&' printf \"$DIR/{1/}\\0\" \
-        | xargs -0 pdfcpu import -c disable "$PDFFILE"
+PDFFILE="$DIR/$NAME - $VOLUME-$NUMBER - $STITLE.pdf"
+if [[ "$MARKED" != "(cached)" ]]; then
+    mapfile -t < <(http "https://api.cdnlibs.org/api/$REQNAME/chapter?volume=$VOLUME&number=$NUMBER" \
+        | jq -r '.data.pages[] | "https://img33.imgslib.link\(.url)\t'"$DIR/"'\(.url | split("/")[-1])"')
+
+    parallel --colsep='\t' -kq http GET {1} "referer:$DOMAIN" -o {2} ::: "${MAPFILE[@]}"
+    pdfcpu import -c disable "$PDFFILE" "${MAPFILE[@]##*$'\t'}"
+    rm "${MAPFILE[@]##*$'\t'}"
 fi
 
 xdg-open "$PDFFILE"
