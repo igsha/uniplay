7da52e66ef2f7463c41107ff4cfa94dad6348ed4
Igor Sharonov <igsha@users.noreply.github.com>
2025-06-13 19:11:58 +0300

    ulst: New fetcher

--- fetchers/onlyurl/remanga.sh
+++ fetchers/onlyurl/remanga.sh
@@ -24,13 +24,21 @@ mark() {
 
 read -r DOMAIN < <(grep -Po '.+//[^/]+' <<< "$URL")
 read -r REQNAME < <(grep -Po '.+//[^/]+/[^/]+/\K[^/]+' <<< "$URL")
+echo "Remanga: Download list https://api.remanga.org/api/v2/titles/$REQNAME/" >&2
 IFS=$'\t' read -r NAME BRANCHID < <(http GET "https://api.remanga.org/api/v2/titles/$REQNAME/" \
     | jq -r '"\(.secondary_name | sub("/"; "-"))\t\(.branches[0].id)"')
 
-IFS=$'\t' read -r MARKED CHAPTER ID < <(http GET "https://api.remanga.org/api/v2/titles/chapters/?branch_id=$BRANCHID&ordering=index" \
-    | jq -r '.results[] | "\(.chapter)\t\(.id)"' \
-    | mark "$DIR/$NAME" \
-    | fzf)
+echo "Remanga: Download chapters https://api.remanga.org/api/v2/titles/chapters/?branch_id=$BRANCHID&ordering=index" >&2
+APIURL="https://api.remanga.org"
+ID=next
+CHAPTER="/api/v2/titles/chapters/?branch_id=$BRANCHID&ordering=index"
+while [[ "$ID" == next ]]; do
+    URL="$APIURL$CHAPTER"
+    IFS=$'\t' read -r MARKED CHAPTER ID < <(http GET "$URL" \
+        | jq -r '(.results + [.next | select(. != null) | {chapter: ., id: "next"}]) | .[] | [.chapter, .id] | @tsv' \
+        | mark "$DIR/$NAME" \
+        | fzf)
+done
 
 DIR="$DIR/$NAME/$ID"
 mkdir -p "$DIR"
@@ -46,13 +54,13 @@ if [[ "$MARKED" != "(cached)" ]]; then
     done < <(http GET "https://api.remanga.org/api/v2/titles/chapters/$ID/" \
         | jq -r '.pages | flatten | .[] | "\(.link)\t'"$IMGTMPDIR/"'\(.link | split("/")[-1])"')
 
-    echo Extract pages... >&2
+    echo Remanga: Extract pages... >&2
     parallel --colsep='\t' -kq http GET {1} "referer:$DOMAIN" -o {2} ::: "${URLS[@]}" :::+ "${FILES[@]}"
-    echo Convert pages... >&2
+    echo Remanga: Convert pages... >&2
     magick \( "${FILES[@]}" -append \) -crop 1x${#FILES[@]}@ +repage "$IMGTMPDIR"/replaced_%04d.jpg >&2
-    echo Create pdf... >&2
+    echo Remanga: Create pdf... >&2
     pdfcpu import -c disable "$PDFFILE" "$IMGTMPDIR"/replaced_*.jpg >&2
-    echo Remove pages... >&2
+    echo Remanga: Remove pages... >&2
     rm -r "$IMGTMPDIR"
 fi
 
