631e32d344496f564d3266fd6a95e5f48d09d375
Igor Sharonov <igsha@users.noreply.github.com>
2025-10-22 22:24:49 +0300

    aaf21f422339a9526f2e3099a5937249: Support listing

--- fetchers/aaf21f422339a9526f2e3099a5937249.sh
+++ fetchers/aaf21f422339a9526f2e3099a5937249.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+shopt -s lastpipe
 
 which jq http htmlq > /dev/null
 
@@ -9,6 +10,27 @@ read -r URL < <(jq -r .url <<< "${JSON[@]}")
 read -r REGISTER < <(mktemp -t uniplayer.aaf2.XXX)
 trap "rm \"$REGISTER\"" INT EXIT
 http --follow --timeout 5 GET "$URL" > "$REGISTER"
+
+while [[ "$URL" =~ /models/ ]]; do
+    < "$REGISTER" \
+        htmlq '.list-videos a.thumb_title' \
+        | rg 'href="([^"]+)".*title="([^"]+)"' -or $'$1\t$2' \
+        | readarray -t LIST
+
+    if < "$REGISTER" \
+        htmlq '#list_videos_common_videos_list_pagination li.next > a' -a href \
+        | read -r NEXTURL; then
+        awk -F/ '{printf "%s//%s\n", $1, $3}' <<< "$URL" \
+            | read -r DOMAIN
+        LIST+=("$DOMAIN$NEXTURL"$'\t'"next")
+    fi
+
+    printf "%s\n" "${LIST[@]}" \
+        | fzf -d $'\t' --with-nth=2 --accept-nth=1 \
+        | read -r URL
+
+    http --follow --timeout 5 GET "$URL" > "$REGISTER"
+done
 
 mapfile -t URLS < <(htmlq 'video > source' -a src < "$REGISTER")
 [[ "${#URLS[@]}" -ne 0 ]] || { echo "ERROR: Empty urls"; exit 1; }
