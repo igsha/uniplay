948468d9467c175f35443acc1aa8a782f8aa171d
Igor Sharonov <igsha@users.noreply.github.com>
2025-11-15 21:51:56 +0300

    makesel: Use hash of item as primary key

--- fetchers/marksel.sh
+++ fetchers/marksel.sh
@@ -2,7 +2,7 @@
 set -e
 shopt -s lastpipe
 
-which jq sqlite3 fzf > /dev/null
+which jq jo sqlite3 fzf md5sum cut > /dev/null
 
 mapfile -t JSON
 <<< "${JSON[@]}" jq -r '.items[]' \
@@ -17,24 +17,24 @@ mapfile -t JSON
 DB="$XDG_CACHE_HOME/uniplay.db"
 
 echo "marksel: Find info [$TBLNAME] from db $DB" >&2
-sqlite3 "$DB" "create table if not exists '$TBLNAME' (name str);" >&2
-sqlite3 "$DB" "select name from '$TBLNAME';" \
-    | mapfile -t NAMESINTABLE
+sqlite3 "$DB" "create table if not exists '$TBLNAME' (hash char(32));" >&2
+sqlite3 "$DB" "select hash from '$TBLNAME';" \
+    | mapfile -t HASHES
 
 for i in "${!ITEMS[@]}"; do
     MARK="(new)"
-    if [[ " ${NAMESINTABLE[*]} " =~ " ${NAMES[$i]} " ]]; then
+    <<< "${ITEMS[$i]}" md5sum | cut -c -32 | read -r ITEMHASH
+    if [[ " ${HASHES[*]} " =~ " $ITEMHASH " ]]; then
         MARK="(seen)"
     fi
 
-    printf '%s\t%s\t%s\n' "$MARK" "${NAMES[$i]}" "${ITEMS[$i]}"
+    printf '%s\t%s\t%s\t%s\n' "$MARK" "${NAMES[$i]}" "$ITEMHASH" "${ITEMS[$i]}"
 done \
-    | fzf -d $'\t' --with-nth=1,2 --accept-nth=2,3 \
-    | IFS=$'\t' read -r NAME ITEM
+    | fzf -d $'\t' --with-nth=1,2 --accept-nth=2,3,4 \
+    | IFS=$'\t' read -r NAME HASH ITEM
 
 
 echo "marksel: Update db $DB" >&2
-sqlite3 "$DB" "insert into '$TBLNAME' (name) values ('$NAME');" >&2
+sqlite3 "$DB" "insert into '$TBLNAME' (hash) values ('$HASH');" >&2
 
-export NAME ITEM
-<<< "${JSON[@]}" jq '.item=env.ITEM | .title=env.NAME | del(.names) | del(.items)'
+jo result=list item="$ITEM" title="$NAME"
