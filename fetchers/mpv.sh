4eece4654aefcea99c8a9bd1bd7e7a10140b0d0d
Igor Sharonov <igsha@users.noreply.github.com>
2025-11-20 17:35:37 +0300

    rutube: Support chapters

--- fetchers/mpv.sh
+++ fetchers/mpv.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 set -e
+shopt -s lastpipe
 
 which jq mpv http > /dev/null
 
-ARGS=()
 mapfile -t JSON
-if ! read -r ARGS[0] < <(jq -r '.item // empty' <<< "${JSON[@]}"); then
-    readarray -t ARGS < <(jq -r '.items[]' <<< "${JSON[@]}")
-    [[ "${#ARGS[@]}" -gt 0 ]]
-fi
+<<< "${JSON[@]}" jq -r '.item // .items[]' \
+    | readarray -t ARGS
+
+[[ "${#ARGS[@]}" -gt 0 ]]
 echo "mpv: Extract ${ARGS[@]}" >&2
 
-if read -r REFERER < <(jq -r '.referer // empty' <<< "${JSON[@]}"); then
+if <<< "${JSON[@]}" jq -r '.referer // empty' | read -r REFERER; then
     ARGS+=("--http-header-fields=Referer:$REFERER")
 fi
-if read -r TITLE < <(jq -r '.title // empty' <<< "${JSON[@]}"); then
+
+if <<< "${JSON[@]}" jq -r '.title // empty' | read -r TITLE; then
     ARGS+=("--title=$TITLE")
 fi
-if read -r SUBURL < <(jq -r '.subsurl // empty' <<< "${JSON[@]}"); then
-    read -r REGISTER < <(mktemp -t uniplay.mpv.XXX)
-    trap "rm \"$REGISTER\"" INT EXIT
-    echo "Downloading subtitles $SUBURL to $REGISTER"
+
+TEMPS=()
+if <<< "${JSON[@]}" jq -r '.subsurl // empty'| read -r SUBURL; then
+    mktemp -t uniplay.mpv.XXX \
+        | read -r REGISTER
+    TEMPS+=("$REGISTER")
+
+    echo "mpv: Download subtitles $SUBURL to $REGISTER" >&2
     http --follow GET "$SUBURL" > "$REGISTER"
     ARGS+=("--sub-file=$REGISTER")
+fi
+
+if <<< "${JSON[@]}" jq -r '(.chapters // [])[] | "[CHAPTER]\nTIMEBASE=1/1000\nSTART=\(.start)\nEND=\(.end)\ntitle=\(.text)\n"' \
+    | readarray CHAPTERS && [[ "${#CHAPTERS[@]}" -gt 0 ]]; then
+    mktemp -t uniplay.mpv.XXX \
+        | read -r CHAPTERSFILE
+    TEMPS+=("$CHAPTERSFILE")
+
+    echo "mpv: Store chapters in $CHAPTERSFILE" >&2
+    echo ";FFMETADATA1" > "$CHAPTERSFILE"
+    printf "%s" "${CHAPTERS[@]}" >> "$CHAPTERSFILE"
+
+    ARGS+=("--chapters-file=$CHAPTERSFILE")
+fi
+
+if [[ "${#TEMPS[@]}" -gt 0 ]]; then
+    trap "rm ${TEMPS[@]}" INT EXIT
 fi
 
 mpv "${ARGS[@]}"
