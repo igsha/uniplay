c2b2dc6ddaae0a75ac70c8f23cf64b1f60780e21
Igor Sharonov <igsha@users.noreply.github.com>
2025-12-02 22:40:36 +0300

    mangaonelove: Refactor marksel usage

--- fetchers/mangaonelove.sh
+++ fetchers/mangaonelove.sh
@@ -2,32 +2,19 @@
 set -e
 shopt -s lastpipe
 
-which jq http jo sed htmlq grep > /dev/null
+which jq http jo sed htmlq grep xq xargs tee > /dev/null
 
 jq -r .item \
-    | read -r URL
-
-http --follow GET "$URL" \
-    | mapfile HTML
-
-<<< "${HTML[@]}" htmlq '.wp-manga-chapter a' -a href \
-    | jo -a \
-    | read -r URLS
-
-<<< "${HTML[@]}" htmlq '.wp-manga-chapter a' -t \
-    | sed '/^$/d;s/^[[:blank:]]*//;s/[[:blank:]]*$//' \
-    | jo -a \
-    | read -r NAMES
-
-<<< "${HTML[@]}" htmlq title -t \
-    | read -r TITLE
-
-jo result=urls items="$URLS" names="$NAMES" title="$TITLE" \
+    | xargs -I{} -o http --follow GET "{}" \
+    | htmlq 'ul.main.version-chap' --remove-nodes .c-new-tag \
+    | xq '[.. | objects | select(has("@class") and (.["@class"] | startswith("wp-manga-chapter"))) | .a] |
+          {items: map(.["@href"]),
+           names: map(.["#text"]),
+           title: "mangaonelove"}' \
     | "$UNIPLAY" -f marksel \
     | jq -r .item \
-    | read -r URL
-
-http GET "$URL" \
+    | tee >(awk '{printf "mangaonelove: Extract chapter %s\n", $0}' >&2) \
+    | xargs -I{} -o http GET "{}" \
     | htmlq '#chapter_preloaded_images' -t \
     | grep -Po '\[.+\]' \
     | jo result=urls items=:- \
