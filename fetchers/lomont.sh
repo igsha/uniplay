538d50e8ac40f74c18206f327d3f31e02c30e610
Igor Sharonov <igsha@users.noreply.github.com>
2026-01-04 20:04:51 +0300

    lomont: New fetcher

---
+++ fetchers/lomont.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+shopt -s lastpipe
+
+jq -r .item \
+    | read -r URL
+
+echo "lomont: Download $URL" >&2
+http GET "$URL" \
+    | htmlq -t "#inputData" \
+    | mapfile JSON
+
+# rearrange
+<<< "${JSON[@]}" jq 'to_entries | map(.value | to_entries | map(.value) | flatten) | flatten | group_by(.voice_id)' \
+    | mapfile JSON
+
+<<< "${JSON[@]}" jq '{items: map(.[-1] | {item: .voice_id, name: "\(.voice_name) \(.season)-\(.episode)"}), title: "lomont"}' \
+    | "$UNIPLAY" -f selector \
+    | jq -r .item \
+    | read -r VOICE_ID
+
+echo "lomont: Selected voice id $VOICE_ID" >&2
+<<< "${JSON[@]}" jq --arg vid "$VOICE_ID" --arg base "https://lomont.site/player/responce.php?video_id=" \
+    '.[] | select(.[0].voice_id == $vid) |
+    {items: (map({item: "\($base)\(.video_id)", name: "\(.season)-\(.episode)"}) | reverse), title: "lomont"}' \
+    | "$UNIPLAY" -f marksel \
+    | jq -r '.item, .title' \
+    | { read -r URL; read -r TITLE; }
+
+echo "lomont: Download seria $URL" >&2
+export TITLE
+http GET "$URL" \
+    | jq '{item: .src, title: env.TITLE} + ({subsurl: .subtitles.ru?} // {})' \
+    | "$UNIPLAY" -f mpv
