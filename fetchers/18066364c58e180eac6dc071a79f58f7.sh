763707b661cfcbacbe71d0e7fde30a810ab77dd5
Igor Sharonov <igsha@users.noreply.github.com>
2026-03-21 22:24:22 +0300

    18066364c58e180eac6dc071a79f58f7: Avoid non-working resolution

--- fetchers/18066364c58e180eac6dc071a79f58f7.sh
+++ fetchers/18066364c58e180eac6dc071a79f58f7.sh
@@ -14,7 +14,7 @@ if [[ "$URL" =~ /movie/[0-9]+ ]]; then
         | sed -e '1i<div>' -e '$a</div>' \
         | xq -r '.div | {title: .title, items: .div.a | map({url: .["@href"], quality: .["#text"]}) | reverse}' \
         | tee >(jq -r '.items[] | "18066364c58e180eac6dc071a79f58f7: [\(.quality)] \(.url)"' >&2) \
-        | jq '{item: .items[0].url, title: .title}' \
+        | jq '{item: .items | map(select(.url | startswith("https://lolo") | not)) | .[0].url, title: .title}' \
         | exec "$UNIPLAY" -f mpv
 else
     echo "18066364c58e180eac6dc071a79f58f7: List $URL" >&2
