72ef81d8428a2552bee7ad475ddb2672df7aa00a
Igor Sharonov <igsha@users.noreply.github.com>
2026-03-30 18:33:37 +0300

    manga-shi: Fix new API

--- fetchers/manga-shi.sh
+++ fetchers/manga-shi.sh
@@ -6,24 +6,35 @@ which jq jo > /dev/null
 
 mapfile -t JSON
 printf "%s\n" "${JSON[@]}" \
-    | jq -r .item \
-    | read -r URL
+    | jq -r '.item, (.item | split("/")[:3] | join("/"))' \
+    | { read -r URL; read -r DOMAIN; }
 
 if [[ "$URL" =~ [^/]+://[^/]+/manga/[^/]+/?$ ]]; then
     TBLNAME="${URL%/}"
     TBLNAME="${TBLNAME##*/}"
-    echo "manga-shi: Choose chapter for [$TBLNAME]" >&2
 
-    <<< "${JSON[@]}" "$UNIPLAY" -f manga-shi-list \
-        | "$UNIPLAY" -f marksel \
-        | jq -r '.item' \
-        | read -r URL
+    URL="${URL%/}/chapters/?chapter_sort=latest"
+    while [[ "$URL" =~ /chapters/ ]]; do
+        echo "manga-shi: List chapters $URL" >&2
+        http --follow GET "$URL" \
+            | htmlq  \
+            | xq --arg base "$DOMAIN" '.html.body
+                | if (.div != null) then .a += [{"@href": .div.["@hx-get"], span: [{span: "###next###"}]}] end | {
+                    items: .a | map({item: $base + .["@href"], name: .span[0].span}),
+                    title: "manga-shi"
+                }' \
+            | "$UNIPLAY" -f marksel \
+            | jq -r '.item, .title' \
+            | { read -r URL; read -r TITLE; }
+    done
 fi
 
-echo "manga-shi: Download chapter $URL" >&2
-jo result=url item="$URL" \
-    | "$UNIPLAY" -f manga-shi-chapter \
-    | jq '.parallel=4' \
+echo "manga-shi: Download chapter $URL [$TITLE]" >&2
+http --follow GET "$URL" \
+    | htmlq '.reader-pages .reader-page img' \
+    | sed -e 's;\(<img.*\)>;\1/>;g' -e '1i<div>' -e '$a</div>' \
+    | xq --arg base "$DOMAIN" --arg title "$TITLE" \
+        '.div.img | {items: map(.["@data-src"] // .["@src"] | $base + .), title: $title, parallel: 4}' \
     | "$UNIPLAY" -f download \
     | "$UNIPLAY" -f create-pdf \
     | "$UNIPLAY" -f pdf
