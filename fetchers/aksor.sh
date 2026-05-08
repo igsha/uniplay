be1f045dd0bccf77b057679b1a74b6d514bfdc7e
Igor Sharonov <igsha@users.noreply.github.com>
2026-05-08 20:29:53 +0300

    aksor: New fetcher

---
+++ fetchers/aksor.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+shopt -s lastpipe
+
+mapfile JSON
+<<< "${JSON[@]}" jq -r .url \
+    | read -r URL
+
+if [[ ! "$URL" =~ /api/video && "$URL" =~ (https?://[^/]+)/(video/[a-z0-9]+) ]]; then
+    URL="${BASH_REMATCH[1]}/api/${BASH_REMATCH[2]}"
+fi
+
+{
+    http GET "$URL" \
+        | jq -r '{
+            url: (.qualities | with_entries(select(.value != null)) | .q4k // .q2k // .q1080 // .q720 // .q480 // .q360),
+            title: "\(.studio_name) - \(.episode)",
+            type: "video"
+        }'
+    <<< "${JSON[@]}" jq 'del(.url)'
+} | jq -s add
