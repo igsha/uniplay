769c4e6cfdf20783f5f269d33c82de836b9c2969
Igor Sharonov <igsha@users.noreply.github.com>
2026-06-05 17:50:33 +0300

    habr: Fix font size

--- fetchers/habr.sh
+++ fetchers/habr.sh
@@ -21,12 +21,12 @@ http GET "$URL" \
     | sed -E 's;(<img[^>]+height=")[^"]+;\1auto;g' \
     | mapfile HTML
 
-mktemp -u -t uniplay.habr.XXX \
+mktemp -u -t uniplay.habr.XXX.pdf \
     | read -r TMPHABRPDF
 
 echo "habr: Convert to pdf $TMPHABRPDF" >&2
 <<< "${HTML[@]}" pandoc -f html -t pdf -s --reference-links \
-    -V margin-top=5 -V margin-left=5 -V margin-right=5 -V margin-bottom=5 -V "maxwidth:95%" -V papersize=A4 \
+    -V margin-top=10 -V margin-left=10 -V margin-right=10 -V margin-bottom=10 -V "maxwidth:100%" -V papersize=a4 -V fontsize=1.5em \
     --pdf-engine=wkhtmltopdf -o "$TMPHABRPDF"
 
 jo file="$TMPHABRPDF" delete="$TMPHABRPDF" type=pdf
