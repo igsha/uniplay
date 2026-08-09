c006d6c4ebbb5f9c3d810100d3ac2440ce43ed0a
Igor Sharonov <igsha@users.noreply.github.com>
2026-08-09 20:02:19 +0300

    aniboom: Fix list episodes

--- fetchers/aniboom.sh
+++ fetchers/aniboom.sh
@@ -7,25 +7,7 @@ which jq http htmlq xq >/dev/null
 jq -r '.url, (.url | split("/")[:3] | join("/"))' \
     | { read -r URL; read -r DOMAIN; }
 
-if [[ "$URL" =~ episode=list && "$URL" =~ animego_id=([0-9]+) ]]; then
-    ANIMEID="${BASH_REMATCH[1]}"
-    SCHEDULE="https://animego.me/anime/${ANIMEID}/9999999/schedule/load"
-    echo "aniboom: List episodes $SCHEDULE" >&2
-
-    http GET "$SCHEDULE" referer:https://animego.me X-Requested-With:XMLHttpRequest \
-        | jq -r .data.content \
-        | htmlq  --remove-nodes button,span \
-        | htmlq \
-        | xq --arg url "$URL" '.html.body.div | to_entries | group_by(.key / 4 | trunc) | map(.[2].value + .[3].value | select(.["#text"]? | not) | {
-                url: (.["@data-number"] as $num | $url | sub("episode=list"; "episode=\($num)")),
-                title: .["@data-number"]
-            }) | {
-                list: .,
-                hashkey: "url",
-                type: "selectable",
-                title: "aniboom"
-            }'
-elif [[ "$URL" =~ episode=([0-9]+) ]]; then
+if [[ "$URL" =~ episode=([0-9]+) ]]; then
     EPISODE="${BASH_REMATCH[1]}"
     echo "aniboom: Extract episode $EPISODE $URL" >&2
     http GET "$URL" "referer:$DOMAIN" \
@@ -41,4 +23,25 @@ elif [[ "$URL" =~ episode=([0-9]+) ]]; then
             type: "video",
             title: "\($title) - \($num)"
         }'
+elif [[ "$URL" =~ animego_id=([0-9]+) ]]; then
+    ANIMEID="${BASH_REMATCH[1]}"
+    SCHEDULE="https://animego.me/anime/${ANIMEID}/9999999/schedule/load"
+    echo "aniboom: List episodes $SCHEDULE" >&2
+
+    http GET "$SCHEDULE" referer:https://animego.me X-Requested-With:XMLHttpRequest \
+        | jq -r .data.content \
+        | htmlq  --remove-nodes button,span \
+        | htmlq \
+        | xq --arg url "$URL" '.html.body.div | to_entries | group_by(.key / 4 | trunc) | map(.[2].value + .[3].value | select(.["#text"]? | not) | {
+                url: (.["@data-number"] as $num | "\($url)&episode=\($num)"),
+                title: .["@data-number"]
+            }) | {
+                list: .,
+                hashkey: "url",
+                type: "selectable",
+                title: "aniboom"
+            }'
+else
+    echo "aniboom: No anime_id" >&2
+    exit 1
 fi
