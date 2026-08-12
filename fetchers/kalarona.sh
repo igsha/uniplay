156f21397a7ea9b785a3d31ed29368f4681a41fe
Igor Sharonov <igsha@users.noreply.github.com>
2026-08-12 19:47:14 +0300

    kalarona: Fix new api

--- fetchers/kalarona.sh
+++ fetchers/kalarona.sh
@@ -2,18 +2,14 @@
 set -eo pipefail
 shopt -s lastpipe
 
-which jq http htmlq >/dev/null
+which jq http htmlq grep >/dev/null
 
 jq -r .url \
     | read -r URL
 
 http GET "$URL" \
-    | mapfile HTML
-
-parsedata() {
-    htmlq '#inputData' -t \
-        | jq 'to_entries | map(.value | to_entries | map(.value)) | flatten'
-}
+    | grep -Po 'window.playerData = \K{.*(?=;</script>)' \
+    | mapfile JSON
 
 if [[ "$URL" =~ voice=([0-9]+) ]]; then
     VOICE="${BASH_REMATCH[1]}"
@@ -21,45 +17,41 @@ if [[ "$URL" =~ voice=([0-9]+) ]]; then
         SEASON="${BASH_REMATCH[1]}"
         if [[ "$URL" =~ episode=[0-9]+ ]]; then
             echo "kalarona: Extract video from $URL" >&2
-            <<< "${HTML[@]}" htmlq .flowplayer -a data-config \
-                | jq '{
-                    url: .hls,
+            <<< "${JSON[@]}" jq '{
+                    url: .config.video,
                     type: "video",
-                    title: "kalarona"
+                    title: (.playlist | "\(.current.serialName) - \(.serial.current.season)-\(.serial.current.episode)")
                 }'
         else
             echo "kalarona: Select episode from $URL" >&2
-            <<< "${HTML[@]}" parsedata \
-                | jq --argjson ss "$SEASON" --argjson vv "$VOICE" --arg url "$URL" 'map(select(.season == $ss and .voice_id == $vv) | {
-                        url: "\($url)&episode=\(.episode)",
-                        title: .episode
-                    }) | {
-                        list: .,
-                        type: "selectable",
-                        title: "kalarona"
-                    }'
-        fi
-    else
-        echo "kalarona: Select season from $URL" >&2
-        <<< "${HTML[@]}" parsedata \
-            | jq --argjson vv "$VOICE" --arg url "$URL" 'map(select(.voice_id == $vv)) | unique_by(.season) | map({
-                    url: "\($url)&season=\(.season)",
-                    title: .season
+            <<< "${JSON[@]}" jq --argjson ss "$SEASON" --argjson vv "$VOICE" --arg url "$URL" '.playlist.serial.list[$ss - 1] | map({
+                    url: "\($url)&episode=\(.num)",
+                    title: "\(.num)"
                 }) | {
-                    list: .,
+                    list: (. | reverse),
                     type: "selectable",
                     title: "kalarona"
                 }'
-    fi
-else
-    echo "kalarona: Select voice from $URL" >&2
-    <<< "${HTML[@]}" parsedata \
-        | jq --arg url "$URL" 'map({voice_name, voice_id}) | unique_by(.voice_id) | map({
-                url: "\($url)&voice=\(.voice_id)",
-                title: .voice_name
+        fi
+    else
+        echo "kalarona: Select season from $URL" >&2
+        <<< "${JSON[@]}" jq --argjson vv "$VOICE" --arg url "$URL" '.playlist.serial.list | keys | map({
+                url: "\($url)&season=\(. + 1)",
+                title: "\(. + 1)"
             }) | {
-                list: .,
+                list: (. | reverse),
                 type: "selectable",
                 title: "kalarona"
             }'
+    fi
+else
+    echo "kalarona: Select voice from $URL" >&2
+    <<< "${JSON[@]}" jq --arg url "$URL" '.voices | to_entries | map({
+            url: (((select($url | contains("?")) | "&") // "?") as $sep | "\($url)\($sep)voice=\(.key)"),
+            title: .value
+        }) | {
+            list: .,
+            type: "selectable",
+            title: "kalarona"
+        }'
 fi
